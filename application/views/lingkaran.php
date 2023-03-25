030b46216d6a0617ca103ec01fbc0e439deb75d5
muhammadferdiansyahdarmawan <ferdidarmawan469@gmail.com>
2023-03-25 17:50:35 +0700

    first commit

---
+++ application/views/lingkaran.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webprograming 2 | Luas Lingkaran</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow p-3">
+  <div class="container text-white justify-content-center">
+    <class="navbar-brand"><b>Muhammad Ferdiansyah Darmawan | 17200351 | 17.6J.01</b>
+  </div>
+  <br>
+</nav>
+    <div class="card m-4 bg-primary text-white">
+  <div class="card-header text-center">
+    Menghitung Luas Lingkaran (L= Phi*Jari*Jari)
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      Nilai Phi = 3.14 <br>
+      Nilai Jari = 8 <br>
+      Luasnya = ?
+    </li>
+    <li class="list-group-item">
+      <?php
+      $phi = 3.14;
+      $jari = 8;
+      echo '
+        Nilai phi = ' . $phi . ' </br>
+        Nilai jari = ' . $jari . '</br>
+        Luasnya = ' . $phi * $jari * $jari . '</br>';
+      ?></div>
+    </li>
+    </div>
+  </div>
+</body>
+</html>
